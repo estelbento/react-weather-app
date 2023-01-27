bf550d01aeaa3e88f38a29ff1f539b33adefdec1
estelbento <108758649+estelbento@users.noreply.github.com>
2023-01-27 11:30:26 +0000

    added the github repo url

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/estelbento/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced code
+        </a>{" "}
+        by Estela Bento
+      </footer>
+    </div>
+  );
 }
