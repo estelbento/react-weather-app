48b60aabae85d9902fed4e91f1f253e9abb17dee
estelbento <108758649+estelbento@users.noreply.github.com>
2023-01-30 18:39:11 +0000

    added the weather forecast

---
+++ src/WeatherForecast.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">thu</div>
+          <div className="WeatherForecast-icon">icon</div>
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19°</span>{" "}
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
