3a40df736feb1c951acf70782b72db3df4c40e31
estelbento <108758649+estelbento@users.noreply.github.com>
2023-01-31 10:37:38 +0000

    added api to the forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,16 @@
+import axios from "axios";
 import React from "react";
 import "./WeatherForecast.css";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(respose) {}
+
+  let apiKey = "4c49ad1f04ba82365a5494866toe342e";
+  let city = props.city;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
